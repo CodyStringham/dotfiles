031c9ce3ead0c83ec2c563a564090c6c863e30b2
Cody Stringham <cody.stringham@mx.com>
2022-05-23 07:34:58 -0600

    repatched fonts

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -41,7 +41,7 @@ telescope.setup {
             preview_cutoff = 120,
         },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules", ".git/", "dist/", ".DS*", "coverage" },
+        file_ignore_patterns = { "node_modules", ".git/", "dist/", ".DS*", "coverage", ".gem" },
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "absolute" },
         winblend = 0,
