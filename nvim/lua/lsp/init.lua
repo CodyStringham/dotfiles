9b43e50d08ff98c75a5e53b2017f5a2964e38a7b
Cody Stringham <codys@hey.com>
2022-07-02 12:32:42 -0600

    update keymap

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -31,12 +31,12 @@ vim.api.nvim_set_keymap("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<
 vim.api.nvim_set_keymap("n", "<Leader>en", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 
 local on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gf", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd",          "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gf",          "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "K",           "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>rn",  "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>fmt", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>ca',  "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
