d77a7f49d77d32f3d716040e1bbd5117d701b0a9
Cody Stringham <cs.negativ@gmail.com>
2025-02-03 10:56:20 -0700

    Adds mini-surround

--- nvim/lua/plugins/git-signs.lua
+++ nvim/lua/plugins/git-signs.lua
@@ -5,12 +5,12 @@ return {
 
     git_signs.setup({
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+        add = { text = '▌' },
+        change = { text = '▌' },
+        delete = { text = '▌' },
+        topdelete = { text = '▌' },
+        changedelete = { text = '▌' },
+      }
     })
   end
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -40,8 +40,16 @@ return {
     vim.keymap.set('n', '<C-g>', builtin.git_status, { desc = 'Search Git Status' })
     vim.keymap.set('n', '<C-t>', builtin.buffers, { desc = 'Search buffers' })
     vim.keymap.set('n', '<C-.>', builtin.oldfiles, { desc = 'Search Recent' })
-    vim.keymap.set('n', '<leader>F', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
