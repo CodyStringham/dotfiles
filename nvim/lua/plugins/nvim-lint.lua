15d3601cac835e0c6fc342df4dbf6b0e93e8fa5c
Cody Stringham <cs.negativ@gmail.com>
2025-02-06 22:37:18 -0700

    Adds telescope keymaps

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -7,32 +7,26 @@ return {
     'javascriptreact',
     "vue",
   },
-  opts = {
-    linters_by_ft = {
+  config = function()
+    local lint = require('lint')
+
+    lint.linters_by_ft = {
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       vue = { 'eslint_d' },
-      erb = { 'erb-lint' }
-    },
-  },
-  config = function(_, opts)
-    local lint = require('lint')
-    lint.linters_by_ft = opts.linters_by_ft
+      erb = { 'erb-lint' },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({
       "BufEnter",
       "BufWritePost",
       "InsertLeave",
-      "TextChanged",
-      "TextChangedI"
     }, {
+      group = lint_augroup,
       callback = function()
         lint.try_lint()
-      end
-    })
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      callback = function()
         lint.try_lint("cspell")
       end
     })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -12,10 +12,7 @@ return {
       end,
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-    {
-      'nvim-tree/nvim-web-devicons',
-      enabled = vim.g.have_nerd_font
-    },
+    { 'nvim-tree/nvim-web-devicons',                  enabled = false },
   },
   config = function()
     require('telescope').setup {
@@ -25,8 +22,12 @@ return {
         },
       },
       defaults = {
+        disable_devicons = true,
         file_ignore_patterns = {
-          "cmaps/"
+          "cmaps/",
+          "*.csv",
+          "*.yml",
+          "vendor/*"
         }
       },
       pickers = {
@@ -51,6 +52,12 @@ return {
       end
     end
 
+    local get_selection = function()
+      return vim.fn.getregion(
+        vim.fn.getpos ".", vim.fn.getpos "v", { mode = vim.fn.mode() }
+      )
+    end
+
     vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find Files' })
     vim.keymap.set('n', '<C-g>', builtin.git_status, { desc = 'Find Git Status' })
     vim.keymap.set('n', '<C-t>', builtin.buffers, { desc = 'Find buffers' })
@@ -62,8 +69,16 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
     vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+
+    -- Rails
     vim.keymap.set('n', '<Leader>frm', find_files_with({ cwd = "./app/models" }), { desc = 'Rails Modesl' })
     vim.keymap.set('n', '<Leader>frc', find_files_with({ cwd = "./app/controllers" }), { desc = 'Rails Controllers' })
     vim.keymap.set('n', '<Leader>frv', find_files_with({ cwd = "./app/views" }), { desc = 'Rails Views' })
+
+    -- Find Words
+    vim.keymap.set('n', '<C-f>', builtin.grep_string, { desc = "Find current word under cursor"})
+    vim.keymap.set('v', '<C-f>', function()
+      return builtin.grep_string { default_text = table.concat(get_selection())}
+    end, { desc = 'Find word' })
   end
 }
